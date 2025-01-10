e651f4ba6084c1ebd1abc26997b77ef1ba9f651e
Mohamed Naiz <mohamed.naiz@outlook.com>
2025-01-10 11:31:48 +0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Mohamed Naiz on 10-1-25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
